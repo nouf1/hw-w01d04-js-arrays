7ac2b46bba514995bfa379246b9f6101cfa8b402
نوف الوزير <noufalmatroudi@nwfs-MacBook-Air.local>
2019-10-03 06:16:38 +0300

    done

---
+++ homework.js
@@ -0,0 +1,22 @@
+5-function addDashes(){
+     var test = ['test1', 'test2', 'test3'] 
+     document.write(test.join("-"));
+    }
+ //4-
+ function reverseString(wrd) {
+    return wrd.revers;
+}   
+//1-
+function sumNumber(n){
+    for (i=1 ; i<=n ; i++){
+    console.log((i*(i+1))%2)};
+}
+//2
+function sumNumber(n){
+    for (i=1 ; i<=n; i++){
+        if (i%2===0){
+    console.log((i*(i+2))%2)}
+}
+};
+
+
